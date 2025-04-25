afef62e85275acf8ea4ec464d766bf90d71aadc6
BoyanYotov <bojan.jotov@gmail.com>
2025-04-25 08:11:56 +0300

    Delete Demo2.java

--- src/MultidimentionalArrays_02/Demo2.java
+++
@@ -1,9 +0,0 @@
-package MultidimentionalArrays_02;
-
-import java.util.Scanner;
-
-public class Demo2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}

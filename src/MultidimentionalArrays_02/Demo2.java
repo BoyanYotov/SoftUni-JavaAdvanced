46e8a5de2188e19cbb68e5ff8a65935f844de1ae
BoyanYotov <bojan.jotov@gmail.com>
2025-04-25 08:10:49 +0300

    New file

---
+++ src/MultidimentionalArrays_02/Demo2.java
@@ -0,0 +1,9 @@
+package MultidimentionalArrays_02;
+
+import java.util.Scanner;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
